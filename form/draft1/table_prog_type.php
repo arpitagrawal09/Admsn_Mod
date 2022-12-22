4d40a6fb0a57f5bade49bfc8484339c33b30d075
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-22 13:11:12 +0530

    unknown commit

---
+++ form/draft1/table_prog_type.php
@@ -0,0 +1,11 @@
+<?php
+//Author:Arpit Agrawal  Created:22/12/2022
+//Created the table prog_types
+
+//get the connection
+require_once "connection.php";
+
+$sql="CREATE TABLE prog_type(
+    
+);"
+?>
