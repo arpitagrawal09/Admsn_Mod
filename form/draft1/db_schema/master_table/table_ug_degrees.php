0ebd56326e9f73d1a31b9b82ed8e3d7f82724b00
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-23 15:49:36 +0530

    untracked commit

--- form/draft1/db_schema/master_table/table_ug_degrees.php
+++ form/draft1/db_schema/master_table/table_ug_degrees.php
@@ -5,8 +5,29 @@
 //get the connection
 require_once "connection.php";
 
-/* $sql="CREATE TABLE `prog_type`(
-    `id` tinyint(4) PRIMARY KEY NOT NULL,
-    type varchar(25) NOT NULL
-);"  */
+$sql="CREATE TABLE `degrees`(
+    `id` SMALLINT(9) PRIMARY KEY NOT NULL,
+    `name` VARCHAR(163) NOT NULL, 
+     `type` TINYINT(4) NOT NULL,
+     FOREIGN KEY `type` REFERENCES prog_type(`type`)
+);
+
+/* CREATE TABLE `degree_types`(
+    `id` TINYINT(5) PRIMARY KEY NOT NULL,
+    `type` VARCHAR(39) NOT NULL
+    FOREIGN KEY (`id`) REFERENCES `degrees`(`type`)
+); */    
+
+INSERT INTO `degrees` VALUES (1, 'B.A.','Under),
+                            (2, 'B.A.(Hons.)')
+                            (5, 'B.Sc.'),
+                            (6, 'B.Sc.(Hons.)')
+                            (31, 'M.A.')
+                            (32, 'M.A.(Journalism and Mass Communication)')
+                            (46, 'M.B.A.(Human Resource Management)')
+                            (47, 'M.B.A.(International Business)')
+";  
+$result=$conn->query($sql);
+if($result) echo "Table degree_types created and filled successfully";
+else echo "Error in creation of the table degree_types";
 ?>
