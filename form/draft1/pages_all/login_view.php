1d1124f6d8379ec26366802c5ea56b1106e976cc
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-23 09:58:25 +0530

    untracked commit

--- form/draft1/pages_all/login_view.php
+++ form/draft1/pages_all/login_view.php
@@ -1,17 +1,35 @@
 <!DOCTYPE html>
 <HTML>
     <head>
-        <title><marquee>Admission Login!</marquee></title>
+        <link rel="stylesheet" href="login.css">
+        <script>
+            function validate(){
+                var username=document.getElementById("username");
+                var password=document.getElementById("password");
+                if(username!="username"){
+                    alert("Please enter the correct username");
+                    return false;
+                }else if(password!="password"){
+                    alert("Please enter the correct password");
+                    return false;
+                }else return true;
+            }
+        </script>
+        <title>Login IIS University!</title>
     </head>
     <body>
+        <div class="heading1" id="heading1LoginPage">
+            <marquee>Admission for the programmes is open!</marquee>
+        </div>
+        
         <form action="login_controller.php" method="POST">
-        <div class="loginBox">
+            <div class="loginBox">
             <h1>LOGIN</h1>
             <br>
             <label id="program">Select Program</label>
             <select id="program" name="program">
                 <?php
-                    require_once "API.php";
+                    /* require_once "API.php";
                     $recordAPI=new API();
                     $allProg=$recordAPI->getAllProg();
                     foreach($allProg as ){
@@ -21,7 +39,7 @@
                     $prog_name=$prog['name'];
                     echo"
                         <option value=>".
-                    ";
+                    "; */
                 ?>
                 <option value="1">Undergraduate</option>
                 <option value="2">Postgraduate</option>
@@ -33,8 +51,8 @@
             <label id="password">Password</label>
             <input type="text" id="password" name="password">
             <br><br>
-            <button type="sumbit">Login</button>
-        </div>
+            <button type="sumbit" id="buttonLogin" name="buttonLogin" onclick="validate()">Login</button>
+            </div>
         </form>
       
     </body>
