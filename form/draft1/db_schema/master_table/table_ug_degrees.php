9faab10f9e1ad3de68dd41aeda19ef62d4866249
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-23 16:02:54 +0530

    untracked commit

--- form/draft1/db_schema/master_table/table_ug_degrees.php
+++ form/draft1/db_schema/master_table/table_ug_degrees.php
@@ -9,8 +9,8 @@ $sql="CREATE TABLE `degrees`(
     `id` SMALLINT(9) PRIMARY KEY NOT NULL,
     `name` VARCHAR(163) NOT NULL, 
      `type` TINYINT(4) NOT NULL,
-     FOREIGN KEY `type` REFERENCES prog_type(`type`)
-);
+     FOREIGN KEY (`type`) REFERENCES `prog_type`(`type`)
+);".
 
 /* CREATE TABLE `degree_types`(
     `id` TINYINT(5) PRIMARY KEY NOT NULL,
@@ -18,14 +18,14 @@ $sql="CREATE TABLE `degrees`(
     FOREIGN KEY (`id`) REFERENCES `degrees`(`type`)
 ); */    
 
-INSERT INTO `degrees` VALUES (1, 'B.A.','Under),
-                            (2, 'B.A.(Hons.)')
-                            (5, 'B.Sc.'),
-                            (6, 'B.Sc.(Hons.)')
-                            (31, 'M.A.')
-                            (32, 'M.A.(Journalism and Mass Communication)')
-                            (46, 'M.B.A.(Human Resource Management)')
-                            (47, 'M.B.A.(International Business)')
+"INSERT INTO `degrees` VALUES (1, 'B.A.', 1),
+                            (2, 'B.A.(Hons.)', 1),
+                            (5, 'B.Sc.', 1),
+                            (6, 'B.Sc.(Hons.)', 1),
+                            (31, 'M.A.', 2),
+                            (32, 'M.A.(Journalism and Mass Communication)', 2),
+                            (46, 'M.B.A.(Human Resource Management)', 2),
+                            (47, 'M.B.A.(International Business)', 2)
 ";  
 $result=$conn->query($sql);
 if($result) echo "Table degree_types created and filled successfully";
