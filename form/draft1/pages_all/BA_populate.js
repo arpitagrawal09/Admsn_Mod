307cd58b3a99755a564f786d5841cb1739ce32b7
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-23 11:46:44 +0530

    Untracked commit

---
+++ form/draft1/pages_all/BA_populate.js
@@ -0,0 +1,17 @@
+function populateBAPass(){
+    var allElectives=Array();
+    allElectives[0]=['English, History, GPEM(AP307)'];
+    allElectives[1]=['Psychology, Public Ad, Geography(AP132)'];
+    allElectives[2]=['Cinematographics, Journalism, MotionPicture(AP213)'];
+}
+
+$("document").onload(function(){
+    $("#idDdnProgType").onchange(function(){
+        var progType=document.getElementById("idDdnProgType").value;
+        switch(progType){
+            case "Undergraduate":
+                $("UGSection").show();
+                $("PGSection").hide();
+        }
+    });
+});
