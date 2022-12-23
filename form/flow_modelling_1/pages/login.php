3fa34bd36987b09105d9d7af68a96d06b15abfd2
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-23 06:17:59 +0530

    untracked commit

--- form/flow_modelling_1/pages/login.php
+++ form/flow_modelling_1/pages/login.php
@@ -1,20 +1,24 @@
 <!DOCTYPE html>
 <HTML>
     <head>
-        <link rel="stylesheet" href="all.css">
+        <link rel="stylesheet" href="login.css">
         <title>Admission Login!</title>
     </head>
     <body>
         <br>
         <marquee>Admissions for all the programs are open!</marquee>
+        <br>
         <form action="pd.php" method="POST">
             <div class="loginBox">
-                <br>
-                <label id="username">Username</label>
-                <input type="text" id="username" name="username">
+                <div id=#username>
+                    <label id="username">Username</label>
+                    <input type="text" id="username" name="username">
+                </div>
                 <br><br>
-                <label id="password">Password</label>
-                <input type="text" id="password" name="password">
+                <div>
+                    <label id="password">Password</label>
+                    <input type="text" id="password" name="password">
+                </div>
                 <br><br>
                 <button type="submit" id="loginButton" name="loginButton">Login</button>
             </div>
