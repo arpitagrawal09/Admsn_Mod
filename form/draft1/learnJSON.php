4183c2aff5e2f168651b2ed64867125a66b510f0
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-25 10:13:50 +0530

    untracked commit

---
+++ form/draft1/learnJSON.php
@@ -0,0 +1,8 @@
+<?php 
+//File to start learning JSON as if from the beginning
+
+//Fruits JSON
+$fruitJSONManual = '{"apple":200, "banana":40, "orange":85}';
+$fruitArr=array("apple"=>200,"banana"=>40, "orange"=>85 );
+echo $fruitJSON;
+?>
