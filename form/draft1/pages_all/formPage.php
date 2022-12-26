28da123705911da89052417d9fe9739055a194eb
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-26 09:15:19 +0530

    untracked commit

--- form/draft1/pages_all/formPage.php
+++ form/draft1/pages_all/formPage.php
@@ -10,17 +10,56 @@ if(isset($_POST)&&!empty($_POST)){
         $_SESSION['user']['password']=$_POST['password'];
     if(!empty($_POST['program']))
         $_SESSION['user']['program']=$_POST['program'];
-}else echo "Error in receiving the student login details/program";
+}//else echo "Error in receiving the student login details/program";
 ?>
 
 <HTML>
     <head>
         <title>Admission Form</title>
-        <link rel="stylesheet" href="pd.css">
+        <link rel="stylesheet" href="formPage.css">
         <script src="jquery-3.6.1.min.js"></script>
-        <script src="libraries.js"></script>
-        <script src="BA_populate.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>        
+        <script src="jquery-3.6.1.min.js"></script>
+<!--         <script src="formPage.js"></script>  -->
         <script>
+            /*$("document").ready(function(){
+                $("#idDdnProgType").onchange(function(){
+                    var progType=document.getElementById("idDdnProgType").value;
+                    switch(progType){
+                        case "Undergraduate":
+                        $("UGSection").show();
+                        $("PGSection").hide();
+                    }
+                })
+            }); */
+
+            $(document).ready(function(){
+                $("select").change(function(){
+                    var val=$("select").val();
+                    alert("Value"+val+"selected");
+                    populateElecDdn(this);
+                })
+            });
+
+            function populateDdnElecComb(val){
+                var elecCombBA;
+                elecCombBA=['AP01','AP04','AP72'];
+                var reqObj=new XMLHttpRequest();
+                var recdJSON=reqObj.responseText;
+                reqObj.open();
+                reqObj.send("GET","",true);
+                switch(val){
+                    case 1:
+
+                    case 2:
+                        
+                    default:
+                        break;
+                }
+            }
 
         </script>
     </head>
@@ -45,8 +84,9 @@ if(isset($_POST)&&!empty($_POST)){
                 <div>
              
                 <div class="divCourseSel divDdn" id="idCourseSel" name="nameCourseSel">
-                    <div class="row degreeRow">                              
-                        <div class="col-sm-2 divDdn">
+                    <div class="row combRow">                              
+                        
+                        <div class="col-sm-2 divDdnDegree">
                             <label for="ddnDegree">Select Degree</label>
                             <select id="ddnDegree" name="ddnDegree">
                                 <?php
@@ -55,6 +95,15 @@ if(isset($_POST)&&!empty($_POST)){
                                 ?>
                             </select>        
                         </div>
+
+                        <div class="col-sm-2 ddn">
+                            <label for="idDdnElecComb">
+                                <select id="idDdnElecComb" name="nameDdnElecComb">
+
+                                </select>
+                            </label>
+                        </div>
+
                     </div>
                 </div>  
 
