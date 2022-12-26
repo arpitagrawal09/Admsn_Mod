ba7337b98518b647fe10ab86a5427c5f6a2c87b3
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-26 16:49:25 +0530

    committing the `lead_creation` table

---
+++ form/draft1/db_schema/master_table/lead_create.php
@@ -0,0 +1,20 @@
+<?php
+//Author:Arpit Agrawal  Created:26/12/2022
+//File to create the table for the generated lead db
+
+require_once "connnection.php";
+
+$sql="CREATE TABLE `lead`(
+        `id` INT(32) PRIMARY KEY AUTO_INCREMENT NOT NULL, 
+        `online_offline` TINYINT(4) NOT NULL, 
+        `username` VARCHAR(171), 
+        `password` VARCHAR(31),
+        `photo`
+        `form_degree_set` TINYINT(7) NOT NULL, 
+        `form_no` INT(32) NOT NULL, 
+        `reg_no` INT(32) NOT NULL, 
+        `student_name` VARCHAR(171) NOT NULL, 
+        `program` UNSIGNED SMALLINT(15) NOT NULL, 
+
+    );";
+?>
