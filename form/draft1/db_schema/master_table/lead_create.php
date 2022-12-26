a5aff2c479aebef8c6389dc255b9801ec7f698db
arpitagrawal09 <arpitagrawal09@gmail.com>
2022-12-26 21:08:07 +0530

    modified lead_create.php

--- form/draft1/db_schema/master_table/lead_create.php
+++ form/draft1/db_schema/master_table/lead_create.php
@@ -7,14 +7,17 @@ require_once "connnection.php";
 $sql="CREATE TABLE `lead`(
         `id` INT(32) PRIMARY KEY AUTO_INCREMENT NOT NULL, 
         `online_offline` TINYINT(4) NOT NULL, 
-        `username` VARCHAR(171), 
-        `password` VARCHAR(31),
-        `photo`
+        `username` VARCHAR(171) NOT NULL, 
+        `password` VARCHAR(31) NOT NULL,
+        `email` VARCHAR(91) NOT NULL, 
+        `photo` NOT NULL, 
+        `mobile` TINYINT(8) NOT NULL, 
+
         `form_degree_set` TINYINT(7) NOT NULL, 
         `form_no` INT(32) NOT NULL, 
         `reg_no` INT(32) NOT NULL, 
         `student_name` VARCHAR(171) NOT NULL, 
-        `program` UNSIGNED SMALLINT(15) NOT NULL, 
-
+        `program` UNSIGNED SMALLINT(15) NOT NULL,
+        `created` 
     );";
 ?>
