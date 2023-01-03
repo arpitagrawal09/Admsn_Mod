72a050edc2a7874a6afc109cf4ebd223bcb178e4
arpitagrawal09 <arpitagrawal09@gmail.com>
2023-01-03 15:41:09 +0530

    untracked commit

--- form/draft1/pages_all/master_data.php
+++ form/draft1/pages_all/master_data.php
@@ -5,8 +5,10 @@
     $progMasterArr=array();
     $sql="SELECT * FROM `prog_type`";
     $progMasterTable=$conn->query($sql);
-    if($progMasterTable) echo "Master table prog_type successfully fetched<br>";
-    else "Error in getting master table prog_type<br>";
+    //if($progMasterTable) 
+    //    echo "Master table prog_type successfully fetched<br>";
+    //else 
+        //echo "Error in getting master table prog_type<br>";
     //echo "<PRE>";
     if($progMasterTable){
         while($prog=$progMasterTable->fetch_row()){
