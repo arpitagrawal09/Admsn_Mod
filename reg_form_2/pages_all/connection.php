f1609858300aa26eec207ced086513afd6330581
arpitagrawal0909 <arpit.agrawal@iisuniv.ac.in>
2023-01-04 20:51:06 +0530

    connection no addition

--- reg_form_2/pages_all/connection.php
+++ reg_form_2/pages_all/connection.php
@@ -7,7 +7,5 @@ $conn=new mysqli(DB_servername, DB_username, DB_password, DB_db);
     die( "Error in connecting the database");
 <<<<<<<< HEAD:form/branch_offc_maybe/pages_all/connection.php
 else echo "Database connected successfully<br>"; */
-========
-//else echo "Database connected successfully<br>";
->>>>>>>> 2d3432e7e5edea9fa897b817c2998f3c4f678a72:reg_form/pages_all/connection.php
+
 ?>
