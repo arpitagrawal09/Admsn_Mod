d0b49aaff682db5b9240a73fd752e36a9b81aa56
arpitagrawal09 <arpitagrawal09@gmail.com>
2023-01-04 16:00:45 +0530

    mergin all repos

---
+++ merged/merged/pages_all/API.php
@@ -0,0 +1,22 @@
+<?php
+//Author:Arpit Agrawal  Created:22/12/2022
+//API that exposes the CRUD queries on the database 
+require_once "connection.php";
+
+class API{
+
+    function getAllProg(){
+        require_once "connection.php";
+        $query="SELECT * FROM `prog_type`";
+        $allProg=$conn->query($query);
+        if($allProg){
+            while($oneProg=$allProg->fetch_row()){
+                print_r($oneProg);
+            }
+        }
+    }
+}
+
+$APITest = new API();
+$APITest->getAllProg();
+?>
---
+++ merged/merged/pages_all/divAllDetailsForm.php
@@ -0,0 +1,7 @@
+<?php
+//Author:Arpit Agrawal  Created:23/12/2022
+//File having the divs for the form sections. Helps writing code
+echo '    
+
+';
+?>
