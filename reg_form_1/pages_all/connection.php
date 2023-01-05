8e6d0ff6fb7bcf4f1726d43291421052aaa66015
arpitagrawal0909 <arpit.agrawal@iisuniv.ac.in>
2023-01-05 22:07:55 +0530

    merging two folders of reg

--- reg_form_1/pages_all/connection.php
+++ reg_form_1/pages_all/connection.php
@@ -5,5 +5,7 @@ require_once "configuration.php";
 $conn=new mysqli(DB_servername, DB_username, DB_password, DB_db);
 /* if($conn->connect_error)
     die( "Error in connecting the database");
+<<<<<<<< HEAD:form/branch_offc_maybe/pages_all/connection.php
 else echo "Database connected successfully<br>"; */
+
 ?>
