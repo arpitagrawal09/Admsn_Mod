e1608e515de99cc66090f6209b7c281800587d27
arpitagrawal09 <arpitagrawal09@gmail.com>
2023-01-09 14:36:27 +0530

    untracked

--- reg_form/config/configuration.php
+++ reg_form/config/configuration.php
@@ -2,8 +2,8 @@
 //Author:Arpit Agrawal  Created:20/12/2022
 
 define("DB_username", "root");
-define("DB_password", "");
+define("DB_password", "password");
 define("DB_servername", "localhost");
-define("DB_db", "mock_admission_1"); 
+define("DB_db", "admsn"); 
 
 ?>
